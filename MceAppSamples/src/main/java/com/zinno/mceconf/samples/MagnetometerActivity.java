b434426d8ab2c9e70ca390e2aa42a9277502777e
Sebastian Kacprzak <nait@naitbit.com>
2015-02-04 17:44:23 +0100

    Added: initial version of playing sound depending of magnet distance from the sensor.

--- MceAppSamples/src/main/java/com/zinno/mceconf/samples/MagnetometerActivity.java
+++ MceAppSamples/src/main/java/com/zinno/mceconf/samples/MagnetometerActivity.java
@@ -1,6 +1,10 @@
 package com.zinno.mceconf.samples;
 
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
@@ -23,6 +27,33 @@ import butterknife.InjectView;
 
 
 public class MagnetometerActivity extends BleServiceBindingActivity {
+
+
+    // originally from http://marblemice.blogspot.com/2010/04/generate-and-play-tone-in-android.html
+    // and modified by Steve Pomeroy <steve@staticfree.info>
+    private final int duration = 1; // seconds
+    private final int sampleRate = 8000;
+    private final int numSamples = duration * sampleRate;
+    private final double sample[] = new double[numSamples];
+    private double freqOfTone = 300; // hz
+    private double MAX_FREQ = 3000;
+    private double MIN_FREQ = 400;
+
+    private final byte generatedSnd[] = new byte[2 * numSamples];
+
+    Handler handler = new Handler();
+
+
+
+    AudioTrack audioTrack;
+    
+    
+    
+    
+    
+    
+    
+    
     private static final String TAG = MagnetometerActivity.class.getSimpleName();
     private TiSensor<?> magnetometerSensor;
     private boolean sensorEnabled;
@@ -75,6 +106,12 @@ public class MagnetometerActivity extends BleServiceBindingActivity {
             }
         });
         toolbar.setTitle(R.string.magnetometer_sample_name);
+
+
+        audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+                sampleRate, AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT, generatedSnd.length,
+                AudioTrack.MODE_STREAM);
     }
 
 
@@ -106,6 +143,23 @@ public class MagnetometerActivity extends BleServiceBindingActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        final Thread thread = new Thread(new Runnable() {
+            public void run() {
+                genTone();
+                handler.post(new Runnable() {
+
+                    public void run() {
+                        playSound();
+                    }
+                });
+            }
+        });
+//        thread.start();
+    }
+
+    @Override
     public void onDataAvailable(String deviceAddress, String serviceUuid, String characteristicUUid, String text, byte[] data) {
         Log.d(TAG, String.format("DeviceAddress: %s,ServiceUUID: %s, CharacteristicUUIS: %s", deviceAddress, serviceUuid, characteristicUUid));
         Log.d(TAG, String.format("Data: %s", text));
@@ -120,6 +174,9 @@ public class MagnetometerActivity extends BleServiceBindingActivity {
         } else {
             float value = (magneticValues[2] - environmentCalibrateValue[2]);
 
+            setFrequency(value);
+            
+            
             switch (state) {
                 case LOW:
                     if (lastValue - value > OFFSET) {
@@ -154,6 +211,7 @@ public class MagnetometerActivity extends BleServiceBindingActivity {
     }
 
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -174,5 +232,58 @@ public class MagnetometerActivity extends BleServiceBindingActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+
+
+
+
+
+    private void setFrequency(float value) {
+        if(value== 0) {
+            value = 0.000001f;
+        }
+        double freq = Math.sqrt(1/value);
+        freq = freq * 5400 - 400;
+        Log.w("FREQUENCY", "freq:" + freq);
+
+//        double freq = 1 / (value * value);
+//        double freq = 300+ value;
+        freq = Math.min(MAX_FREQ, freq);
+        freq = Math.max(MIN_FREQ, freq);
+        freqOfTone = freq;
+        audioTrack.pause();
+        audioTrack.flush();
+        genTone();
+        playSound();
+    }
+
+
+    void genTone(){
+        // fill out the array
+        for (int i = 0; i < numSamples; ++i) {
+            sample[i] = Math.sin(2 * Math.PI * i / (sampleRate/freqOfTone));
+        }
+
+        // convert to 16 bit pcm sound array
+        // assumes the sample buffer is normalised.
+        int idx = 0;
+        for (final double dVal : sample) {
+            // scale to maximum amplitude
+            final short val = (short) ((dVal * 32767));
+            // in 16 bit wav PCM, first byte is the low order byte
+            generatedSnd[idx++] = (byte) (val & 0x00ff);
+            generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+
+        }
+    }
+
+    void playSound(){
+        audioTrack.write(generatedSnd, 0, generatedSnd.length);
+        audioTrack.play();
     }
 }
